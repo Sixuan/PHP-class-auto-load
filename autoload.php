16f11da91045bcfdc7d6d5de017edded4572092e
Sixuan <sliu@delivery.com>
2015-05-28 23:43:27 -0400

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,17 @@
+autoload_class();
+
+function autoload_class() {
+    $directories = array(
+        'app/config/',
+        'app/controllers/',
+        'app/models/',
+        'app/lib/contracts/',
+        'app/lib/',
+    );
+    foreach ($directories as $directory) {
+        foreach (glob($directory."*.php") as $filename)
+        {
+            include_once $filename;
+        }
+    }
+}
